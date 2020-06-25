0cbab9069a85fb15395b25d2f7e103749630c752
Aidar Habibullin <36470111+Aidar-Frontend-Developer@users.noreply.github.com>
2020-06-25 15:38:04 +0300

    Add files via upload
    lesson-4: Working with Streams.

---
+++ lesson4/task-3/AccountManager.js
@@ -0,0 +1,17 @@
+const { Writable } = require('stream');
+
+class AccountManager extends Writable {
+  constructor(options = { objectMode: true }) {
+    super(options);
+    this.data = [];
+  }
+
+  _write(chunk, encoding, done) {
+    console.log(chunk.payload);
+
+    this.data.push(chunk);
+    done();
+  }
+}
+
+module.exports = AccountManager;
