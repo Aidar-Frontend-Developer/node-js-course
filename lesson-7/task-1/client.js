e9dd64fdd35f072ff8a8c076103eaa7222a4c005
Aidar-Frontend-Developer <zoomme.kzn@gmail.com>
2020-07-10 20:05:48 +0300

    lesson-7:  Implemented data sampling module.

---
+++ lesson-7/task-1/client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+
+const client = new net.Socket();
+
+client.connect(8080, () => {
+	client.write(
+		JSON.stringify({
+			name: {
+				first: "John",
+				last: "d",
+			},
+			phone: "56",
+			address: {
+				zip: "1234",
+				city: "Kyiv",
+				country: "ukr",
+				street: "so",
+			},
+			email: "@gmail.com",
+		})
+	);
+});
+
+client.on("data", (data) => {
+	console.log(data.toString());
+});
+
+client.on("close", () => {
+	console.log("Connection closed");
+});
