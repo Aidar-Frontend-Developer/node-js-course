65f59ffe2d5daf4ff2b184a30ed29e265cf84488
Aidar Habibullin <zoomme.kzn@gmail.com>
2020-11-08 03:37:12 +0300

    lesson-21-task-1: Queries

--- express/source/routers/users/route.js
+++ express/source/routers/users/route.js
@@ -1,16 +1,22 @@
-export const getUsers = (req, res) => {
-    try {
-        const data = [];
+// Instruments
+import { UsersController } from '../../controllers';
 
-        res.status(200).json({ data });
+export const getUsers = async (req, res) => {
+    try {
+        const { page = 1, size = 10 } = req.query;
+        const model = new UsersController({ page, size });
+        const data = await model.getAll();
+
+        res.status(200).json({ ...data });
     } catch ({ message }) {
         res.status(400).json({ message });
     }
 };
 
-export const addUser = (req, res) => {
+export const addUser = async (req, res) => {
     try {
-        const data = {};
+        const model = new UsersController(req.body);
+        const data = await model.create();
 
         res.status(201).json({ data });
     } catch ({ message }) {
