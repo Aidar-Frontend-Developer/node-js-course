88e9bfbdc7e6c3f5dbd2346110e5a625573ea986
Aidar Habibullin <36470111+Aidar-Frontend-Developer@users.noreply.github.com>
2020-11-08 03:46:01 +0300

    lesson-21-task-2: Implement queries for users hash requests (#35)

--- express/source/routers/users/hash/route.js
+++ express/source/routers/users/hash/route.js
@@ -1,25 +1,33 @@
-export const getUserByHash = (req, res) => {
-    try {
-        const data = {};
+// Instruments
+import { UsersController } from '../../../controllers';
 
+export const getUserByHash = async (req, res) => {
+    try {
+        const { userHash } = req.params;
+        const model = new UsersController({ hash: userHash });
+        const data = await model.getByHash();
         res.status(200).json({ data });
     } catch ({ message }) {
         res.status(400).json({ message });
     }
 };
 
-export const updateUserByHash = (req, res) => {
+export const updateUserByHash =  async (req, res) => {
     try {
-        const data = {};
-
+        const { userHash } = req.params;
+        const model = new UsersController({ hash: userHash, payload: req.body });
+        const data = await model.updateByHash();
         res.status(200).json({ data });
     } catch ({ message }) {
         res.status(400).json({ message });
     }
 };
 
-export const removeUserByHash = (req, res) => {
+export const removeUserByHash = async (req, res) => {
     try {
+        const { userHash } = req.params;
+        const model = new UsersController({ hash: userHash });
+        await model.removeByHash();
         res.sendStatus(204);
     } catch ({ message }) {
         res.status(400).json({ message });
