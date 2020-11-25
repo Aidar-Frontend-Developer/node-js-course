47bc050574ce2aa2f5fda4fb0faf70fb4c346af9
Aidar Habibullin <zoomme.kzn@gmail.com>
2020-11-25 11:29:33 +0300

    lesson-23: Validation

--- express/source/odm/lessons.js
+++ express/source/odm/lessons.js
@@ -3,46 +3,60 @@ import mongoose from 'mongoose';
 import v4 from 'uuid/v4';
 
 const contentSchema = new mongoose.Schema({
-    title: String,
-    order: Number,
-    uri:   String,
-});
-
-const schema = new mongoose.Schema({
     title: {
-        type:     String,
-        required: true,
-    },
-    description: {
-        type:     String,
-        required: true,
+        type:      String,
+        maxlength: 30,
     },
     order: {
-        type:     Number,
-        required: true,
+        type: Number,
+        min:  0,
     },
-    hash: {
-        type:     String,
-        required: true,
-        unique:   true,
-        default:  () => v4(),
-    },
-    availability: [
-        {
-            type: String,
-            enum: [ 'standard', 'select', 'premium' ],
-        },
-    ],
-    content: {
-        videos:   [ contentSchema ],
-        keynotes: [ contentSchema ],
-    },
-}, {
-    timestamp: {
-        createdAt: 'created',
-        updatedAt: 'modified',
+    uri: {
+        type:  String,
+        match: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
     },
 });
+
+const schema = new mongoose.Schema(
+    {
+        title: {
+            type:      String,
+            required:  true,
+            maxlength: 30,
+        },
+        description: {
+            type:      String,
+            required:  true,
+            maxlength: 250,
+        },
+        order: {
+            type:     Number,
+            required: true,
+        },
+        hash: {
+            type:     String,
+            required: true,
+            unique:   true,
+            default:  () => v4(),
+        },
+        availability: [
+            {
+                type: String,
+                enum: [ 'standard', 'select', 'premium' ],
+            },
+        ],
+        content: {
+            videos:   [ contentSchema ],
+            keynotes: [ contentSchema ],
+        },
+    },
+    {
+        timestamp: {
+            createdAt: 'created',
+            updatedAt: 'modified',
+        },
+    },
+);
 
 schema.index(
     {
--- express/source/odm/users.js
+++ express/source/odm/users.js
@@ -2,73 +2,97 @@
 import mongoose from 'mongoose';
 import v4 from 'uuid/v4';
 
-const schema = new mongoose.Schema({
-    name: {
-        first: {
+const schema = new mongoose.Schema(
+    {
+        name: {
+            first: {
+                type:      String,
+                required:  true,
+                minlength: 2,
+                maxlength: 15,
+            },
+            last: {
+                type:      String,
+                required:  true,
+                minlength: 2,
+                maxlength: 15,
+            },
+        },
+        phones: [
+            {
+                phone: {
+                    type:     String,
+                    required: true,
+                },
+                primary: Boolean,
+            },
+        ],
+        emails: [
+            {
+                email: {
+                    type:     String,
+                    required: true,
+                    unique:   true,
+                    match:    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                },
+                primary: Boolean,
+            },
+        ],
+        password: {
+            type:      String,
+            select:    false,
+            minlength: 10,
+            required:  true,
+        },
+        sex: {
             type:     String,
+            enum:     [ 'm', 'f' ],
             required: true,
         },
-        last: {
+        roles: [
+            {
+                type:    String,
+                default: 'newbie',
+                enum:    [ 'newbie', 'student', 'teacher' ],
+            },
+        ],
+        socials: {
+            facebook: {
+                type:  String,
+                match: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
+            },
+            linkedin: {
+                type:  String,
+                match: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
+            },
+            github: {
+                type:  String,
+                match: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
+            },
+            skype: {
+                type:  String,
+                match: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
+            },
+        },
+        notes: {
+            type:      String,
+            maxlength: 250,
+        },
+        hash: {
             type:     String,
             required: true,
+            unique:   true,
+            default:  () => v4(),
+        },
+        disabled: Boolean,
+    },
+    {
+        timestamp: {
+            createdAt: 'created',
+            updatedAt: 'modified',
         },
     },
-    phones: [
-        {
-            phone: {
-                type:     String,
-                required: true,
-            },
-            primary: Boolean,
-        },
-    ],
-    emails: [
-        {
-            email: {
-                type:     String,
-                required: true,
-                unique:   true,
-            },
-            primary: Boolean,
-        },
-    ],
-    password: {
-        type:     String,
-        select:   false,
-        required: true,
-    },
-    sex: {
-        type:     String,
-        enum:     [ 'm', 'f' ],
-        required: true,
-    },
-    roles: [
-        {
-            type:    String,
-            default: 'newbie',
-            enum:    [ 'newbie', 'student', 'teacher' ],
-        },
-    ],
-    socials: {
-        facebook: String,
-        linkedin: String,
-        github:   String,
-        skype:    String,
-    },
-    notes: String,
-    hash:  {
-        type:     String,
-        required: true,
-        unique:   true,
-        default:  () => v4(),
-    },
-    disabled: Boolean,
-}, {
-    timestamp: {
-        createdAt: 'created',
-        updatedAt: 'modified',
-    },
-});
+);
 
 schema.index({
     'name.first': 1,
